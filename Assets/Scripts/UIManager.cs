5b5f5d657cb9e87c74c1732a781828443a800c20
Hongbeubeu <44913755+Hongbeubeu@users.noreply.github.com>
2023-02-11 20:19:33 +0700

    Update asset

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,85 +3,79 @@ using TMPro;
 
 public enum UIPanel
 {
-    PAUSE,
-    LOSE,
-    PLAY
+	PAUSE,
+	LOSE,
+	PLAY
 }
+
 public class UIManager : MonoBehaviour
 {
-    #region Singleton
-    static UIManager _instance;
+	#region Singleton
 
-    public static UIManager instance
-    {
-        get
-        {
-            if (_instance == null)
-                _instance = FindObjectOfType<UIManager>();
-            return _instance;
-        }
-    }
-    #endregion
-    Transform losePanel,
-              pausePanel,
-              playPanel;
-    TextMeshProUGUI maxScore, currentScore;
-    private void Awake()
-    {
-        Init();
-    }
+	static UIManager _instance;
 
-    private void OnEnable()
-    {
-        SetActivePanel(UIPanel.PLAY);
-    }
+	public static UIManager instance
+	{
+		get
+		{
+			if (_instance == null)
+				_instance = FindObjectOfType<UIManager>();
+			return _instance;
+		}
+	}
 
-    void Init()
-    {
-        losePanel = transform.Find("Lose Panel");
-        pausePanel = transform.Find("Pause Panel");
-        playPanel = transform.Find("Play Panel");
+	#endregion
 
-        maxScore = playPanel.Find("Score Panel/Score Max").GetComponent<TextMeshProUGUI>();
-        currentScore = playPanel.Find("Score Panel/Score Current").GetComponent<TextMeshProUGUI>();
-    }
+	[SerializeField] private GameObject losePanel;
 
-    public void SetActivePanel(UIPanel panel)
-    {
-        ResetPanel();
-        switch (panel)
-        {
-            case UIPanel.PAUSE:
-                pausePanel.gameObject.SetActive(true);
-                break;
-            case UIPanel.LOSE:
-                losePanel.gameObject.SetActive(true);
-                break;
-            case UIPanel.PLAY:
-                playPanel.gameObject.SetActive(true);
-                break;
-        }
-    }
+	[SerializeField] private GameObject pausePanel;
+	[SerializeField] private GameObject playPanel;
 
-    public void SetCurrentScore(int score)
-    {
-        currentScore.SetText("Current: " + score);
-    }
+	[SerializeField] private TextMeshProUGUI maxScore;
+	[SerializeField] private TextMeshProUGUI currentScore;
 
-    public void SetMaxScore(int max)
-    {
-        maxScore.SetText("Max: " + max);
-    }
 
-    void ResetPanel()
-    {
-        losePanel.gameObject.SetActive(false);
-        pausePanel.gameObject.SetActive(false);
-        playPanel.gameObject.SetActive(false);
-    }
+	private void OnEnable()
+	{
+		SetActivePanel(UIPanel.PLAY);
+	}
 
-    public void Quit()
-    {
-        Application.Quit();
-    }
+	public void SetActivePanel(UIPanel panel)
+	{
+		ResetPanel();
+		switch (panel)
+		{
+			case UIPanel.PAUSE:
+				pausePanel.SetActive(true);
+				break;
+			case UIPanel.LOSE:
+				losePanel.SetActive(true);
+				break;
+			case UIPanel.PLAY:
+				playPanel.SetActive(true);
+				break;
+		}
+	}
+
+	public void SetCurrentScore(int score)
+	{
+		currentScore.SetText($"{score}");
+	}
+
+	public void SetMaxScore(int max)
+	{
+		maxScore.SetText($"{max}");
+	}
+
+	void ResetPanel()
+	{
+		losePanel.SetActive(false);
+		pausePanel.SetActive(false);
+		playPanel.SetActive(false);
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
 }
